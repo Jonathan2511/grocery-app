f8904174e7d894b8edf08ca324ec427dfdc16bcf
Jonathan2511 <jonathanaditya2@gmail.com>
2023-12-23 13:27:15 +0700

    Home Page

---
+++ lib/route/app_page.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:get/get.dart';
+import 'package:my_grocery/route/app_route.dart';
+import 'package:my_grocery/view/dashboard/dashboard_binding.dart';
+import 'package:my_grocery/view/dashboard/dashboard_screen.dart';
+
+class AppPage {
+  static var list = [
+    GetPage(
+        name: AppRoute.dashboard,
+        page: () => DashboardScreen(),
+        binding: DashboardBinding())
+  ];
+}
---
+++ lib/view/home/home_screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:my_grocery/component/main_header.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [MainHeader()],
+      ),
+    );
+  }
+}
