57a716605e80c6cb249393587a0f3744b354932b
Jonathan2511 <jonathanaditya2@gmail.com>
2023-12-24 00:47:21 +0700

    Add Popular Category Part 1

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:my_grocery/component/main_header.dart';
 import 'package:my_grocery/controller/controllers.dart';
 import 'package:my_grocery/view/home/components/carousel_slider/carousel_loading.dart';
 import 'package:my_grocery/view/home/components/carousel_slider/carousel_slider_view.dart';
+import 'package:my_grocery/view/home/components/popular_category/popular_category_loading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -22,7 +23,14 @@ class HomeScreen extends StatelessWidget {
             } else {
               return CarouselLoading();
             }
-          })
+          }),
+          Obx(() {
+            if (homeController.popularCategoryList.isNotEmpty) {
+              return PopularCategoryLoading();
+            } else {
+              return PopularCategoryLoading();
+            }
+          }),
         ],
       ),
     );
